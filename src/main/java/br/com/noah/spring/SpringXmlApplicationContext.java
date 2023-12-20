b3c25ca4ab98774eb36ec7fec5f57dc014451871
Bruno Mallia <bmalliadeveloper@gmail.com>
2023-12-20 15:24:01 -0300

    Adding application context from XML

--- src/main/java/br/com/noah/spring/SpringXmlApplicationContext.java
+++ src/main/java/br/com/noah/spring/SpringXmlApplicationContext.java
@@ -1,6 +1,7 @@
 package br.com.noah.spring;
 
-import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import br.com.noah.spring.xml.XmlJdbcConnection;
@@ -9,10 +10,13 @@ import br.com.noah.spring.xml.XmlPersonDao;
 
 public class SpringXmlApplicationContext {
     
+    private static Logger LOGGER = LoggerFactory.getLogger(SpringScanApplication.class);
+
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext applicationContext = 
                 new ClassPathXmlApplicationContext("applicationContext.xml")) {
-
+            
+            LOGGER.info("Beans loaded -> {}", (Object) applicationContext.getBeanDefinitionNames());        
             XmlPersonDao personDao = applicationContext.getBean(XmlPersonDao.class);
             System.out.println(personDao);
 
